304e6660b483fe0a7b0a54bd306d8bbd5430538f
bcd-efg <106222846+bcd-efg@users.noreply.github.com>
2022-05-25 21:03:10 +0530

    Add files via upload

---
+++ KG SM Codes/KG SM Codes/PHP/PHPTEXT.php
@@ -0,0 +1,14 @@
+<html><head>
+<title>READING DATA FROM TEXTFIELDS</title>
+</head>
+<body bgcolor="black">
+<font color="orange" face="Verdana" size="5">
+<h1> READING DATA FROM TEXT FIELDS</h1>
+THANK'S FOR ANSWERING, <br/><b>
+<?php
+echo strtoupper($_REQUEST["text1"]);
+?>
+</b></font>
+<br/><br/><br/>
+<a href="f1.html"><input type="submit" value="BACK"></a>
+</body></html>
